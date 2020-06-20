2826121ee2ad4007aadd4c5e3b124ce5480339eb
ladsfdsfs <nguyen.k.allen@gmail.com>
2020-06-19 22:25:05 -0400

    added quicksort and main running file

--- sorter.py
+++ sorter.py
@@ -1,8 +1,31 @@
+def partition(arr,low,high):  #quicksort helper
+    i = ( low-1 )         
+    pivot = arr[high]     
+  
+    for j in range(low , high): 
+
+        if   arr[j] <= pivot: 
+
+            i = i+1 
+            arr[i],arr[j] = arr[j],arr[i] 
+  
+    arr[i+1],arr[high] = arr[high],arr[i+1] 
+    return ( i+1 ) 
+def quickSort(arr,low,high): 
+    if low < high: 
+
+        pi = partition(arr,low,high) 
+  
+
+        quickSort(arr, low, pi-1) 
+        quickSort(arr, pi+1, high) 
+        return arr
+
 
 class Sorter:
-    def verify_sorted(self, list):
+    def verify_sorted(self, arr):
         placeholder = 0
-        for a in list:
+        for a in arr:
             if placeholder <= a:
                 placeholder = a
                 continue
@@ -10,11 +33,24 @@ class Sorter:
                 return False
         return True
 
-    def selection_sort(self, list):
-        for i in range(len(list)): 
+    def selection_sort(self, arr):
+        for i in range(len(arr)): 
             min_idx = i 
-            for j in range(i+1, len(list)): 
-                if list[min_idx] > list[j]: 
+            for j in range(i+1, len(arr)): 
+                if arr[min_idx] > arr[j]: 
                     min_idx = j 
-            list[i], list[min_idx] = list[min_idx], list[i] 
-        return list
+            arr[i], arr[min_idx] = arr[min_idx], arr[i] 
+        return arr
+
+    def bubble_sort(self, arr):
+        n = len(arr) 
+        for i in range(n-1): 
+            for j in range(0, n-i-1): 
+                if arr[j] > arr[j+1] : 
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+        return arr
+    
+    def quick_sort(self, arr):
+        return quickSort(arr, 0, len(arr)-1)
+
+    
