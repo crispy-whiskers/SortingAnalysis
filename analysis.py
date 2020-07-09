6482ff47361f528b455f9c7522019fca1b72e451
ladsfdsfs <nguyen.k.allen@gmail.com>
2020-07-09 17:57:37 -0400

    finalize data display

--- analysis.py
+++ analysis.py
@@ -1,15 +1,25 @@
 import sorter, timeit
 import numpy as np
 import matplotlib.pyplot as plt
-
+from decimal import Decimal
+n = 10000
+def autolabel(rects):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                '%.2E' % Decimal(height),
+                ha='center', va='bottom')
 
 setup = '''
 import random, sorter
 
-s = [random.randint(1,500) for i in range(10000)]
+s = [random.randint(1,500) for i in range({})]
 sort = sorter.Sorter()
 
-'''
+'''.format(n)
 
 sorts = [name for name, val  in sorter.Sorter.__dict__.items() if callable(val)] 
 times = []
@@ -20,11 +30,15 @@ for alg in sorts:
 results = {k:v for k,v in zip(sorts, times)}
 
 print(results)
-#plt.bar(results.keys(), list(results.values()),  width=.5, color='g' , bottom=.3)
+ind = np.arange(len(results.keys()))
 fig, ax = plt.subplots()
-#ax.set_xticks(rotation=45)
+rects = ax.bar(ind*2, results.values(), .55, color="g")
 ax.set_xticklabels(results.keys())
-rects = ax.bar(range(len(results.values())), results.values(), .35, color="g", bottom=.4)
-plt.xticks(rotation=45)
-ax.set_title('Sorting Algorithm Times for n=10000')
+autolabel(rects)
+plt.xticks(ind*2, rotation=90)
+plt.yticks(np.arange(0, 10, 1))
+ax.set_title('Sorting Algorithm Times for n='+str(n))
+plt.ylabel('Seconds')
+plt.tight_layout()
 plt.show()
+
--- dummy.py
+++ dummy.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from decimal import Decimal
 def autolabel(rects):
     """
     Attach a text label above each bar displaying its height
@@ -7,14 +8,21 @@ def autolabel(rects):
     for rect in rects:
         height = rect.get_height()
         ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
-                '%d' % int(height),
+                '%.2E' % Decimal(height),
                 ha='center', va='bottom')
 
 results = {'selection_sort': 2.5229019999999984, 'bubble_sort': 6.371796200000002, 'quick_sort': 0.029005200000000286, 'merge_sort': 0.04052809999999596, 'radix_sort': 3.4195555999999954, 'insertion_sort': 3.137587499999995, 'heap_sort': 0.048856200000003014, 'cycle_sort': 7.412533699999997, 'pigeonhole_sort': 0.0029794000000009646, 'tim_sort': 0.0007918999999958487}
+ind = np.arange(len(results.keys()))
 fig, ax = plt.subplots()
+
+rects = ax.bar(ind*2, results.values(), .55, color="g")
+
+
 ax.set_xticklabels(results.keys())
-rects = ax.bar(np.arange(len(results.keys())), results.values(), .75, color="g")
 autolabel(rects)
-plt.xticks(rotation=65)
+plt.xticks(ind*2, rotation=90)
+plt.yticks(np.arange(0, 10, 1))
 ax.set_title('Sorting Algorithm Times for n=10000')
+plt.ylabel('Seconds')
+plt.tight_layout()
 plt.show()
